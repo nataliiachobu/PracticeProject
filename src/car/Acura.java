65ed07bda2aecf1f8988d0d58007d21646fdcfd7
nataliiachobu <mebel2008@yahoo.com>
2023-05-03 09:02:02 -0700

    created Honda class

---
+++ src/car/Acura.java
@@ -0,0 +1,7 @@
+package car;
+
+public class Acura extends Car{
+    public Acura(String brand, String make, boolean hasWheels) {
+        super(brand, make, hasWheels);
+    }
+}
---
+++ src/car/Honda.java
@@ -0,0 +1,9 @@
+package car;
+
+public class Honda extends Car{
+    public Honda(String brand, String make, boolean hasWheels) {
+        super(brand, make, hasWheels);
+    }
+
+
+}
