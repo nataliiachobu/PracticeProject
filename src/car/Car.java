3099d3145aec77654227f03d171e8a02ab062d48
nataliiachobu <mebel2008@yahoo.com>
2023-05-03 09:01:08 -0700

    created car class

---
+++ src/car/Car.java
@@ -0,0 +1,14 @@
+package car;
+
+public class Car {
+
+    String brand;
+    String make;
+    boolean hasWheels;
+
+    public Car(String brand, String make, boolean hasWheels) {
+        this.brand = brand;
+        this.make = make;
+        this.hasWheels = hasWheels;
+    }
+}
