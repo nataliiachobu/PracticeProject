1946cee1f6ba0c24695461baf4ef468c08dfdb5f
nataliiachobu <mebel2008@yahoo.com>
2023-05-03 09:09:43 -0700

    created Motocycle extended Car class

--- src/car/Motocycle.java
+++ src/car/Motocycle.java
@@ -1,6 +1,9 @@
 package car;
 
-public class Motocycle  {
+public class Motocycle extends Car {
 
 
+    public Motocycle(String brand, String make, boolean hasWheels) {
+        super(brand, make, hasWheels);
+    }
 }
